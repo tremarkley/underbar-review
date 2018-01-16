87c8bac0dc74b8e80120bad76e2d5f3afb0a268d
tremarkley <tremarkley@gmail.com>
2018-01-15 20:31:05 -0800

    completed 1/4th of bare minimum

--- src/underbar.js
+++ src/underbar.js
@@ -80,12 +80,22 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    let result = [];
+    _.each(collection, function(value, index) { 
+      if (test(value)) {
+        result.push(value);
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+
+    
   };
 
   // Produce a duplicate-free version of the array.
